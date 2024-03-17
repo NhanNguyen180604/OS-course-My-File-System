ef4a97842cfa4b9def44e19b9cd03556bfcbe99a
Putnea <118365947+Putnea@users.noreply.github.com>
2024-03-17 15:59:17 +0700

    Fix deleting file mechanic and updating restore function

--- MyFileSystem2.cpp
+++ MyFileSystem2.cpp
@@ -32,7 +32,7 @@ std::vector<std::pair<std::string, unsigned int>> MyFileSystem::GetFileList() {
 void MyFileSystem::PrintFileList(const std::vector<std::pair<std::string, unsigned int>>& fileList)
 {
     int i = 1;
-    for (std::pair<std::string, int> p: fileList) 
+    for (std::pair<std::string, int> p : fileList)
     {
         std::cout << i++ << ". " << p.first << '\n';
     }
@@ -43,18 +43,16 @@ void MyFileSystem::ListFiles() {
     PrintFileList(fileList);
 }
 
-void MyFileSystem::MyDeleteFile(unsigned int bytesOffset, bool restorable) {
-    if (restorable) {
-        unsigned char deleteValue = 0xE5;
-        unsigned char trueValue;
-        f.seekg(bytesOffset, f.beg);
-        f.read((char*)&trueValue, sizeof(trueValue));
-        f.seekp(bytesOffset + ENTRY_NAME_SIZE + FILE_EXTENSION_LENGTH, f.beg);
-        f.write((char*)&trueValue, sizeof(trueValue));
-        f.seekp(bytesOffset, f.beg);
-        f.write((char*)&deleteValue, sizeof(deleteValue));
-    }
-    else
+void MyFileSystem::DeleteFile(unsigned int bytesOffset, bool restorable) {
+    unsigned char deleteValue = 0xE5;
+    unsigned char trueValue;
+    f.seekg(bytesOffset, f.beg);
+    f.read((char*)&trueValue, sizeof(trueValue));
+    f.seekp(bytesOffset + ENTRY_NAME_SIZE + FILE_EXTENSION_LENGTH, f.beg);
+    f.write((char*)&trueValue, sizeof(trueValue));
+    f.seekp(bytesOffset, f.beg);
+    f.write((char*)&deleteValue, sizeof(deleteValue));
+    if(!restorable)
     {
         f.seekg(bytesOffset, f.beg);
         Entry e;
@@ -75,7 +73,7 @@ void MyFileSystem::MyDeleteFile(unsigned int bytesOffset, bool restorable) {
         }
 
         //find the last entry to overwrite at the current offset
-        
+
     }
     f.flush();
 }
@@ -100,10 +98,13 @@ void MyFileSystem::MyDeleteFile()
     bool restorable;
     std::cout << "Do you want this to be restorable (1 - yes/0 - no): ";
     std::cin >> restorable;
-    MyDeleteFile(bytesOffset, restorable);
+    DeleteFile(bytesOffset, restorable);
 }
 
-void MyFileSystem::MyRestoreFile(std::string restoreName) {
+
+std::vector<std::pair<std::string, unsigned int>> MyFileSystem::GetDeletedFileList()
+{
+    std::vector<std::pair<std::string, unsigned int>> fileList;
     std::vector<unsigned int> rdetClusters = GetClustersChain(STARTING_CLUSTER);
     for (unsigned int cluster : rdetClusters)
     {
@@ -112,45 +113,50 @@ void MyFileSystem::MyRestoreFile(std::string restoreName) {
         unsigned int limitOffset = bytesOffset + bytesPerSector * sectorsPerCluster;  //start of next cluster
         for (; bytesOffset < limitOffset; bytesOffset += sizeof(Entry))
         {
-            std::vector<char> tempEntry = ReadBlock(bytesOffset, sizeof(Entry));
+            Entry tempEntry;
+            f.seekg(bytesOffset, f.beg);
+            f.read((char*)&tempEntry, sizeof(Entry));
             //sign of erased file
-            if (tempEntry[0] == -27) {
-                std::string fileName = "";
-                fileName += tempEntry[62];
-                //get the last index of the file name
-                int finalIndex = 37;
-                while (tempEntry[finalIndex] == ' ') {
-                    finalIndex--;
-                }
-                for (int i = 1; i <= finalIndex; i++) {
-                    fileName += tempEntry[i];
-                }
-                //check duplicate file name;
-                if (tempEntry[39] == '1') {
-                    fileName = fileName + '.' + tempEntry[40] + tempEntry[41] + tempEntry[42];
-                }
-                else {
-                    char duplicateIndex = tempEntry[39] - 1;
-                    fileName = fileName + '(' + duplicateIndex + ')' + '.' + tempEntry[40] + tempEntry[41] + tempEntry[42];
-                }
-                if (fileName != restoreName) {
-                    continue;
-                }
-                else {
-                    f.seekp(bytesOffset, f.beg);
-                    f.write((char*)&tempEntry[62], sizeof(tempEntry[62]));
-                    return;
-                }
+            if (tempEntry.name[0] == -27)
+            {
+                std::string fileName = tempEntry.GetInfo();
+                fileName[0] = tempEntry.reserved[0];
+                fileList.push_back(std::make_pair(fileName, bytesOffset));
             }
             //sign of empty entry
-            if (tempEntry[0] == 0)
-                return;
+            if (tempEntry.name[0] == 0)
+            {
+                return fileList;
+            }
             
         }
     }
+    return fileList;
 }
 
-void MyFileSystem::MyRestoreFile()
-{
+void MyFileSystem::RestoreFile(unsigned int bytesOffset) {
+    f.seekg(bytesOffset + ENTRY_NAME_SIZE + FILE_EXTENSION_LENGTH, f.beg);
+    //get the first char of the name
+    unsigned char trueValue;
+    f.read((char*)&trueValue, sizeof(trueValue));
+    f.seekp(bytesOffset, f.beg);
+    f.write((char*)&trueValue, sizeof(trueValue));
+}
 
+void MyFileSystem::MyRestoreFile() {
+    std::cout << "Restorable Deleted File List:\n";
+    std::vector<std::pair<std::string, unsigned int>> fileList = GetDeletedFileList();
+    PrintFileList(fileList);
+    std::cout << '\n';
+
+    //choose file
+    int choice;
+    do
+    {
+        std::cout << "Enter which file to restore: ";
+        std::cin >> choice;
+    } while (choice <= 0 || choice > fileList.size());
+
+    unsigned int bytesOffset = fileList[choice - 1].second;
+    RestoreFile(bytesOffset);
 }
